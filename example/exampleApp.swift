f972ec010717d3e34c4308bf945aa1384cbb4e49
krhsjung <kr.hs.jung@gmail.com>
2024-07-24 11:20:10 +0900

    Initial example project

---
+++ example/exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleApp.swift
+//  example
+//
+//  Created by 정희석 on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
